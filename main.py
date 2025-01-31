3b63d6724a63541b9a87e2851968225a8c4b7315
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-01-31 17:28:49 +0000

    add decode functionality

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from translate import to_morse
+from translate import to_morse, from_morse
 
 if __name__ == "__main__":
-    input = input("Enter phrase to translate: ")
-    print(to_morse(input))
+    mode = input('Encode, or Decode? ')
+    if mode.lower() == 'encode':
+        input = input("Enter phrase to translate: ")
+        print(to_morse(input))
+    elif mode.lower() == 'decode':
+        morse = input('Enter morse code: ')
+        print(from_morse(morse))
+
--- translate.py
+++ translate.py
@@ -4,3 +4,7 @@ import MorseCodePy as mpc
 def to_morse(input, language="english"):
     morse_string = mpc.encode(input, language=language)
     return morse_string
+
+def from_morse(morse, language='english'):
+    decoded_string = mpc.decode(morse, language)
+    return decoded_string
