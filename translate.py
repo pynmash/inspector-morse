2641d5667584561e90b502d33cd3899b69c203c6
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-03 21:47:48 +0000

    Add docstrings to functions

--- translate.py
+++ translate.py
@@ -1,10 +1,20 @@
 import MorseCodePy as mpc
 
 
-def to_morse(input, language="english"):
+    """Convert a string from english to a string containing morse code.
+
+    Keyword arguments:
+    input -- The string to be converted.
+    language -- The language the input is in (default = "english")
+    """
     morse_string = mpc.encode(input, language=language)
     return morse_string
 
-def from_morse(morse, language='english'):
+    """ Convert a morse code string in to the specified language.
+    
+    Keyword arguments:
+    input -- The string to be converted.
+    language -- The language the input is in (default = "english")
+    """
     decoded_string = mpc.decode(morse, language)
     return decoded_string
