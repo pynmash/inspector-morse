cee0adf33054bf6f5dbb7e058a3bb173b4f272f6
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-03 21:48:14 +0000

    Add type hints

--- translate.py
+++ translate.py
@@ -1,6 +1,7 @@
 import MorseCodePy as mpc
 
 
+def to_morse(input: str , language: str = "english") -> str:
     """Convert a string from english to a string containing morse code.
 
     Keyword arguments:
@@ -10,6 +11,7 @@ import MorseCodePy as mpc
     morse_string = mpc.encode(input, language=language)
     return morse_string
 
+def from_morse(morse: str, language: str = 'english') -> str:
     """ Convert a morse code string in to the specified language.
     
     Keyword arguments:
