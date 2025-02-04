4ef7e5343cca7338e8c3608efba2e36d0c434cbc
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-04 20:13:27 +0000

    tidy up

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     mode: str = input('Encode, or Decode? ')
     if mode.lower() == 'encode':
         input: str = input("Enter phrase to translate: ")
-        print(to_morse(input, ))
+        print(to_morse(input))
     elif mode.lower() == 'decode':
         morse:str = input('Enter morse code: ')
         print(from_morse(morse))
