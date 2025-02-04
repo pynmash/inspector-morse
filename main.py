0162212c5ea9cd5a51e3852004077bb6bdb0ecc5
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-04 23:59:49 +0000

    add program loop and quit option

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from translate import to_morse, from_morse
 
 if __name__ == "__main__":
-    mode: str = input('Encode, or Decode? ')
-    if mode.lower() == 'encode':
-        input: str = input("Enter phrase to translate: ")
-        print(to_morse(input))
-    elif mode.lower() == 'decode':
-        morse:str = input('Enter morse code: ')
-        print(from_morse(morse))
+    while True:
+        mode: str = input('Encode, Decode or Quit? ')
+        if mode.lower() == 'quit':
+            break
+        elif mode.lower() == 'encode':
+            input: str = input("Enter phrase to translate: ")
+            print(to_morse(input))
+        elif mode.lower() == 'decode':
+            morse:str = input('Enter morse code: ')
+            print(from_morse(morse))
 
